fc1dba375142e15ec49429a4c732053802f7739b
tery1234 <shanakadesilva46@gmail.com>
2018-10-22 20:38:07 +0530

    new

---
+++ DataAccessLayer/DataAccessLayer.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccessLayer
+{
+    public class DataAccessLayer
+    {
+        #region Properties & Variable Declarations
+        string connection = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataSet ds;
+
+        #endregion
+
+        #region Properties
+
+        public string Connection
+        {
+            get
+            {
+                return connection;
+            }
+
+            set
+            {
+                connection = value;
+            }      
+        }
+
+
+
+        #endregion
+
+        #region Methods For CRUD Operations
+
+        /// <summary>
+        /// Use for all insertions 
+        /// </summary>
+        /// <param name="spName"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public int Execute_Insert(string spName,string tableName,List<string>fieldNames,object [] values)
+        {
+            
+            try
+            {
+                
+                con = new SqlConnection(connection);
+                cmd = new SqlCommand(spName);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 300;
+
+
+                //Adding parameters
+                cmd.Parameters.Add(tableName);
+                cmd.Parameters.AddRange(values);
+
+                foreach (var fields in fieldNames)
+                {
+                    cmd.Parameters.Add(fields);
+                }
+
+                using (con)
+                {
+                    int result = cmd.ExecuteNonQuery();
+                    return result; //return true or false based on the submition
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                return 0;
+            }
+
+           
+        }
+
+        /// <summary>
+        /// Use for Search options without PARMETERS
+        /// </summary>
+        /// <param name="spName"></param>
+        /// <returns></returns>
+        public DataSet Execute_Select(string spName)
+        {
+            try
+            {
+                con = new SqlConnection(connection);
+                cmd = new SqlCommand(spName);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 300;
+                da = new SqlDataAdapter();
+                ds = new DataSet();
+
+                using (con)
+                {
+                    da.Fill(ds);
+                }
+
+                return ds != null ? ds : null;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+
+            }
+        }
+
+
+        /// <summary>
+        /// Use for Search options with PARMETERS
+        /// </summary>
+        /// <param name="spName"></param>
+        /// <param name="parameters"></param>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public  DataSet Execute_Select(string spName,string tableName,List<string> fieldNames,object [] values)
+        {
+            try
+            {
+                con = new SqlConnection(connection);
+                cmd = new SqlCommand(spName);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 300;
+                da = new SqlDataAdapter();
+                ds = new DataSet();
+
+
+                //Adding parameters
+                cmd.Parameters.Add(tableName);
+                cmd.Parameters.AddRange(values);
+               
+                foreach (var fields in fieldNames)
+                {
+                    cmd.Parameters.Add(fields);
+                }
+
+               
+
+
+                using (con)
+                {
+                    da.Fill(ds);
+                }
+
+                return ds != null ? ds : null;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+                
+            }
+        }
+
+
+        /// <summary>
+        /// To All Updates
+        /// </summary>
+        /// <param name="spName"></param>
+        /// <param name="tableName"></param>
+        /// <param name="fieldNames"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public int Execute_Update(string spName, string tableName, List<string> fieldNames, object[] values)
+        {
+            try
+            {
+                con = new SqlConnection(connection);
+                cmd = new SqlCommand(spName);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 300;
+                
+
+
+                //Adding parameters
+                cmd.Parameters.Add(tableName);
+                cmd.Parameters.AddRange(values);
+
+                foreach (var fields in fieldNames)
+                {
+                    cmd.Parameters.Add(fields);
+                }
+
+
+
+
+                using (con)
+                {
+                    int result = cmd.ExecuteNonQuery();
+                    return result;
+                    
+                }
+
+              
+
+            }
+            catch (Exception)
+            {
+
+                return 0;
+
+            }
+        }
+
+
+        /// <summary>
+        /// Delete with values
+        /// </summary>
+        /// <param name="spName"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public int Execute_Delete(string spName,string table, string FieldName,int value)
+        {
+
+            try
+            {
+                con = new SqlConnection(connection); 
+                cmd = new SqlCommand(spName);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 300;
+
+                //Adding Parameters
+                cmd.Parameters.Add(table);
+                cmd.Parameters.Add(FieldName);
+                cmd.Parameters.Add(value);
+                
+
+
+                using (con)
+                {
+                    int result = cmd.ExecuteNonQuery();
+                    return result;
+                }
+            }
+            catch (Exception)
+            {
+
+                return 0;
+            }
+           
+
+            
+        }
+
+        #endregion
+    }
+}
---
+++ LibraryManagement/Login.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Library.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="LibraryManagement.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        .auto-style2 {
+            height: 29px;
+        }
+        #login {
+            width: 673px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Login" ContentPlaceHolderID="Body_PlaceHolder" runat="server">
+    
+                <form id="login" runat="server">
+                    <div style="max-width:400px; margin:50px 20% 0px 20%; border:1px solid blue; padding:5px;background-color:darkgrey" >
+                        <h2 class="form-signin-heading">User Login</h2>
+                        <label for="txtUserName">User Name</label>
+                        <asp:TextBox ID="txtUserId" CssClass="form-control"   runat="server" placeHolder="Type User Name Here..." required="required" />
+                        <br />
+                        <label for="txtPassword">PassWord</label>
+                        <asp:TextBox ID="txtPassword" CssClass="form-control" TextMode="Password" runat="server" placeholder="Type PassWord Here..." required="required"  />
+                    </div>
+
+                    <div class="custom-checkbox" style="max-width:500px; margin:10px 20% 2px 20%; padding:5px">
+                        <asp:CheckBox ID="chkRememberMe" Text="Remember Me" runat="server" />
+                    </div>
+                    
+                    <div style="max-width:500px; margin:0px 20% 0px 20%; padding:5px">
+                        <asp:Button ID="btnLogin" Text="Login" runat="server" CssClass="btn btn-primary" />
+                    </div>
+                    
+                </form>
+</asp:Content>
